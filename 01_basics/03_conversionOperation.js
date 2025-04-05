633b2c2b0f14147b6d13413f02de4dc3dad8c753
Prashant Jain <prashantjain.code@gmail.com>
2025-04-05 09:23:56 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "prashant"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "anything" => true
+// surprisingly "0" => true
+
+
+let someNumber = 55
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
